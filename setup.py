aca9e922620a8a7123c22c74b1c8824ecad0a0c0
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-06-12 17:20:48 -0500

    include profile and import py3exivbind

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
-
 setup(
-    packages=['uiucprescon.imagevalidate'],
+    packages=[
+        'uiucprescon.imagevalidate',
+        "uiucprescon.imagevalidate.profiles"],
     test_suite="tests",
     namespace_packages=["uiucprescon"],
     setup_requires=['pytest-runner'],
-    # install_requires=['aiohttp', 'importlib_resources'],
+    install_requires=['py3exiv2bind==0.1.0b8'],
     tests_require=['pytest'],
     zip_safe=False,
 )
