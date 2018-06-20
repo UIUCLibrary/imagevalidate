442d6d277eb634ff748d620acf03fe100d88695e
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-06-20 12:19:56 -0500

    add integration tests

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--integration",
+        action="store_true",
+        default=False,
+        help="run integration tests"
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--integration"):
+        # --integration given in cli: do not skip integration tests
+        return
+
+    skip_integration = pytest.mark.skip(
+        reason="skipped integration tests. Use --integration option to run")
+
+    for item in items:
+        if "integration" in item.keywords:
+            item.add_marker(skip_integration)
