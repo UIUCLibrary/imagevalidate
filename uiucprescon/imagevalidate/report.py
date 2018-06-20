06f75be14b031adbf7e9b7f11f8957c159ae3139
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-06-20 12:22:19 -0500

    added issues

--- uiucprescon/imagevalidate/report.py
+++ uiucprescon/imagevalidate/report.py
@@ -1,10 +1,28 @@
-import typing
+# import typing
+from typing import NamedTuple, Optional, Dict, List, Union
+
+
+class Result(NamedTuple):
+    expected: Union[str, None]
+    actual: str
 
 
 class Report:
     def __init__(self) -> None:
-        self._properties: typing.Dict[str, str] = dict()
+        self._properties: Dict[str, Result] = dict()
+        self.filename: Optional[str] = None
+        self._issues: List[str] = list()
 
     @property
     def valid(self) -> bool:
-        return True
+        return len(self._issues) == 0
+
+    @property
+    def issues(self) -> List[str]:
+        return self._issues
+
+    def __str__(self) -> str:
+
+        issue_str = "\n".join(self._issues)
+
+        return "File: {}\n{}".format(self.filename, issue_str)
