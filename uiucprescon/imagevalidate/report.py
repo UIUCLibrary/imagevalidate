ccda75343e0dbaf2679f7b6d4669622b77dd1519
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-06-27 17:42:27 -0500

    issues can be looked up based on category

--- uiucprescon/imagevalidate/report.py
+++ uiucprescon/imagevalidate/report.py
@@ -7,22 +7,32 @@ class Result(NamedTuple):
     actual: str
 
 
+
 class Report:
     def __init__(self) -> None:
         self._properties: Dict[str, Result] = dict()
         self.filename: Optional[str] = None
         self._issues: List[str] = list()
+        self._analysis_data: Dict[str, List[str]] = dict()
 
     @property
     def valid(self) -> bool:
-        return len(self._issues) == 0
+        return len(self._analysis_data.items()) == 0
 
-    @property
-    def issues(self) -> List[str]:
-        return self._issues
+    def issues(self, issue_type=None) -> List[str]:
+        if issue_type:
+            return self._analysis_data.get(issue_type, list())
+        else:
+            issues = []
+            for issue_group in self._analysis_data.values():
+                issues += issue_group
+            return issues
 
     def __str__(self) -> str:
 
-        issue_str = "\n".join(self._issues)
+        issue_str = "\n".join(self.issues())
 
         return "File: {}\n{}".format(self.filename, issue_str)
+
+    def _add_issue(self, issue):
+        self._issues.append(issue)
