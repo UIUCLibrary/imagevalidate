aacb171ebe0be5c5c805698f5b635ecd7f618fa7
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-06-29 15:24:56 -0500

    use full version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     test_suite="tests",
     namespace_packages=["uiucprescon"],
     setup_requires=['pytest-runner'],
-    install_requires=['py3exiv2bind==0.1.0b8'],
+    install_requires=['py3exiv2bind==0.1.0'],
     tests_require=['pytest'],
     zip_safe=False,
 )
