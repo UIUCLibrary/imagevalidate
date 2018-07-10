61ad2c0fc6826a4e2b38da0f97f8a31bf93d22d9
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-07-10 15:24:30 -0500

    use updated image test set

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -11,12 +11,12 @@ import urllib.request
 from tempfile import TemporaryDirectory
 
 # TEST_PATH = os.path.join(os.path.dirname(__file__), "henrytestmetadata")
-SAMPLE_IMAGES = "https://jenkins.library.illinois.edu/userContent/metadata_test_tiffs.tar.gz"
+SAMPLE_IMAGES = "https://jenkins.library.illinois.edu/userContent/metadata_test_images.tar.gz"
 
 
 def download_images(url, destination):
     with TemporaryDirectory() as download_path:
-        print("Downloading {}".format(url))
+        print("Downloading {}".format(url), flush=True)
         urllib.request.urlretrieve(url,
                                    filename=os.path.join(download_path,
                                                          "sample_images.tar.gz"))
@@ -36,7 +36,7 @@ def download_images(url, destination):
 def sample_data():
 
     test_path = os.path.dirname(__file__)
-    sample_images_path = os.path.join(test_path, "henrytestmetadata")
+    sample_images_path = os.path.join(test_path, "metadata_test_images")
 
     if os.path.exists(sample_images_path):
         print("{} already exits".format(sample_images_path))
