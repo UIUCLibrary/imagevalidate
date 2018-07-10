a7fc33471050de2b9866e0a39b1c36cdd1046593
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-07-10 15:13:39 -0500

    paramaterized tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -50,10 +50,13 @@ def sample_data():
 
 
 @pytest.mark.integration
-def test_bitdepth(sample_data):
-    test_image = os.path.join(sample_data, "bitdepth", "0000001.tif")
-    profile_name = imagevalidate.get_profile("HathiTiff")
-    hathi_tiff_profile = imagevalidate.Profile(profile_name)
+@pytest.mark.parametrize("test_file,profile_name", [
+    (os.path.join("bitdepth", "0000001.tif"), "HathiTiff")
+])
+def test_bitdepth(sample_data, test_file, profile_name):
+    test_image = os.path.join(sample_data, test_file)
+    profile_type = imagevalidate.get_profile(profile_name)
+    hathi_tiff_profile = imagevalidate.Profile(profile_type)
     report = hathi_tiff_profile.validate(file=test_image)
     print(report)
     assert len(report.issues()) == 1
@@ -62,10 +65,13 @@ def test_bitdepth(sample_data):
 
 
 @pytest.mark.integration
-def test_colorspace(sample_data):
-    test_image = os.path.join(sample_data, "colorspace", "0000001.tif")
-    profile_name = imagevalidate.get_profile("HathiTiff")
-    hathi_tiff_profile = imagevalidate.Profile(profile_name)
+@pytest.mark.parametrize("test_file,profile_name", [
+    (os.path.join("colorspace", "0000001.tif"), "HathiTiff"),
+])
+def test_colorspace(sample_data, test_file, profile_name):
+    test_image = os.path.join(sample_data, test_file)
+    profile_type = imagevalidate.get_profile(profile_name)
+    hathi_tiff_profile = imagevalidate.Profile(profile_type)
     report = hathi_tiff_profile.validate(file=test_image)
     print(report)
     assert len(report.issues()) == 1
@@ -76,10 +82,13 @@ def test_colorspace(sample_data):
 
 
 @pytest.mark.integration
-def test_correct(sample_data):
-    test_image = os.path.join(sample_data, "correct", "0000001.tif")
-    profile_name = imagevalidate.get_profile("HathiTiff")
-    hathi_tiff_profile = imagevalidate.Profile(profile_name)
+@pytest.mark.parametrize("test_file,profile_name", [
+    (os.path.join("correct", "0000001.tif"), "HathiTiff"),
+])
+def test_correct(sample_data, test_file, profile_name):
+    test_image = os.path.join(sample_data, test_file)
+    profile_type = imagevalidate.get_profile(profile_name)
+    hathi_tiff_profile = imagevalidate.Profile(profile_type)
     report = hathi_tiff_profile.validate(file=test_image)
     print(report)
     assert "No issues discovered" in str(report)
@@ -91,9 +100,13 @@ def test_correct(sample_data):
 
 
 @pytest.mark.integration
-def test_empty_address(sample_data):
-    test_image = os.path.join(sample_data, "empty_address", "0000001.tif")
-    hathi_tiff_profile = imagevalidate.Profile(profiles.HathiTiff())
+@pytest.mark.parametrize("test_file,profile_name", [
+    (os.path.join("empty_address", "0000001.tif"), "HathiTiff"),
+])
+def test_empty_address(sample_data, test_file, profile_name):
+    test_image = os.path.join(sample_data, test_file)
+    profile_type = imagevalidate.get_profile(profile_name)
+    hathi_tiff_profile = imagevalidate.Profile(profile_type)
     report = hathi_tiff_profile.validate(file=test_image)
     print(report)
     assert len(report.issues()) == 1
@@ -104,9 +117,13 @@ def test_empty_address(sample_data):
 
 
 @pytest.mark.integration
-def test_empty_city(sample_data):
-    test_image = os.path.join(sample_data, "empty_city", "0000001.tif")
-    hathi_tiff_profile = imagevalidate.Profile(profiles.HathiTiff())
+@pytest.mark.parametrize("test_file,profile_name", [
+    (os.path.join("empty_city", "0000001.tif"), "HathiTiff"),
+])
+def test_empty_city(sample_data, test_file, profile_name):
+    test_image = os.path.join(sample_data, test_file)
+    profile_type = imagevalidate.get_profile(profile_name)
+    hathi_tiff_profile = imagevalidate.Profile(profile_type)
     report = hathi_tiff_profile.validate(file=test_image)
     assert len(report.issues(issue_type=IssueCategory.INVALID_DATA)) == 0
     assert len(report.issues(issue_type=IssueCategory.EMPTY_DATA)) == 1
@@ -117,10 +134,13 @@ def test_empty_city(sample_data):
 
 
 @pytest.mark.integration
-def test_empty_country(sample_data):
-    test_image = os.path.join(sample_data, "empty_country", "0000001.tif")
-    profile_name = imagevalidate.get_profile("HathiTiff")
-    hathi_tiff_profile = imagevalidate.Profile(profile_name)
+@pytest.mark.parametrize("test_file,profile_name", [
+    (os.path.join("empty_country", "0000001.tif"), "HathiTiff"),
+])
+def test_empty_country(sample_data, test_file, profile_name):
+    test_image = os.path.join(sample_data, test_file)
+    profile_type = imagevalidate.get_profile(profile_name)
+    hathi_tiff_profile = imagevalidate.Profile(profile_type)
     report = hathi_tiff_profile.validate(file=test_image)
     print(report)
     assert len(report.issues()) == 1
@@ -131,10 +151,13 @@ def test_empty_country(sample_data):
 
 
 @pytest.mark.integration
-def test_missing_creator(sample_data):
-    test_image = os.path.join(sample_data, "missing_creator", "0000001.tif")
-    profile_name = imagevalidate.get_profile("HathiTiff")
-    hathi_tiff_profile = imagevalidate.Profile(profile_name)
+@pytest.mark.parametrize("test_file,profile_name", [
+    (os.path.join("missing_creator", "0000001.tif"), "HathiTiff"),
+])
+def test_missing_creator(sample_data, test_file, profile_name):
+    test_image = os.path.join(sample_data, test_file)
+    profile_type = imagevalidate.get_profile(profile_name)
+    hathi_tiff_profile = imagevalidate.Profile(profile_type)
     report = hathi_tiff_profile.validate(file=test_image)
     print(report)
     assert len(report.issues()) == 1
@@ -145,9 +168,13 @@ def test_missing_creator(sample_data):
 
 
 @pytest.mark.integration
-def test_empty_state(sample_data):
-    test_image = os.path.join(sample_data, "empty_state", "0000001.tif")
-    hathi_tiff_profile = imagevalidate.Profile(profiles.HathiTiff())
+@pytest.mark.parametrize("test_file,profile_name", [
+    (os.path.join("empty_state", "0000001.tif"), "HathiTiff"),
+])
+def test_empty_state(sample_data, test_file, profile_name):
+    test_image = os.path.join(sample_data, test_file)
+    profile_type = imagevalidate.get_profile(profile_name)
+    hathi_tiff_profile = imagevalidate.Profile(profile_type)
     report = hathi_tiff_profile.validate(file=test_image)
     assert len(report.issues()) == 1
     assert len(report.issues(issue_type=IssueCategory.INVALID_DATA)) == 0
@@ -158,9 +185,12 @@ def test_empty_state(sample_data):
 
 
 @pytest.mark.integration
-def test_empty_zip(sample_data):
-    test_image = os.path.join(sample_data, "empty_zip", "0000001.tif")
-    profile_name = imagevalidate.get_profile("HathiTiff")
+@pytest.mark.parametrize("test_file,profile_name", [
+    (os.path.join("empty_zip", "0000001.tif"), "HathiTiff"),
+])
+def test_empty_zip(sample_data, test_file, profile_name):
+    test_image = os.path.join(sample_data, test_file)
+    profile_name = imagevalidate.get_profile(profile_name)
     hathi_tiff_profile = imagevalidate.Profile(profile_name)
     report = hathi_tiff_profile.validate(file=test_image)
     print(report)
@@ -172,10 +202,13 @@ def test_empty_zip(sample_data):
 
 
 @pytest.mark.integration
-def test_empty_phonenumber(sample_data):
-    test_image = os.path.join(sample_data, "empty_phonenumber", "0000001.tif")
-    profile_name = imagevalidate.get_profile("HathiTiff")
-    hathi_tiff_profile = imagevalidate.Profile(profile_name)
+@pytest.mark.parametrize("test_file,profile_name", [
+    (os.path.join("empty_phonenumber", "0000001.tif"), "HathiTiff"),
+])
+def test_empty_phonenumber(sample_data, test_file, profile_name):
+    test_image = os.path.join(sample_data, test_file)
+    profile_type = imagevalidate.get_profile(profile_name)
+    hathi_tiff_profile = imagevalidate.Profile(profile_type)
     report = hathi_tiff_profile.validate(file=test_image)
     print(report)
     assert len(report.issues()) == 1
@@ -186,10 +219,13 @@ def test_empty_phonenumber(sample_data):
 
 
 @pytest.mark.integration
-def test_pixelarray(sample_data):
+@pytest.mark.parametrize("test_file,profile_name", [
+    (os.path.join("pixelarray", "0000001.tif"), "HathiTiff"),
+])
+def test_pixelarray(sample_data, test_file, profile_name):
 
-    test_image = os.path.join(sample_data, "pixelarray", "0000001.tif")
-    profile_name = imagevalidate.get_profile("HathiTiff")
+    test_image = os.path.join(sample_data, test_file)
+    profile_name = imagevalidate.get_profile(profile_name)
     hathi_tiff_profile = imagevalidate.Profile(profile_name)
     report = hathi_tiff_profile.validate(file=test_image)
     print(report)
