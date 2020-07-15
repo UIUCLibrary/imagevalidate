72d48e6e40652a601d0d4b19f45a17784152b08d
henry <henryborchers@yahoo.com>
2020-07-15 16:15:46 -0500

    both std=c++14

--- setup.py
+++ setup.py
@@ -1097,7 +1097,7 @@ class BuildPybind11Extension(build_ext):
     def build_extension(self, ext):
         missing = self.find_missing_libraries(ext)
         if self.compiler.compiler_type == "unix":
-            ext.extra_compile_args.append("-std=c++11")
+            ext.extra_compile_args.append("-std=c++14")
         else:
             ext.extra_compile_args.append("/std:c++14")
 
