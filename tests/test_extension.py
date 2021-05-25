3e0a135746f0d38059561d683c2871d41c0700e6
henry <henryborchers@yahoo.com>
2021-05-25 13:47:41 -0500

    register InvalidFileException

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,5 +1,5 @@
 from uiucprescon.imagevalidate import openjp2wrap
-
+import pytest
 
 def test_dummy():
     print(openjp2wrap.open_jpeg_version())
@@ -7,3 +7,7 @@ def test_dummy():
 def test_get_colorspace_exists():
     assert hasattr(openjp2wrap, "get_bitdeph")
 
+def test_invalid_get_bitdeph_throws_exception():
+    source = "fakefile.jp2"
+    with pytest.raises(openjp2wrap.InvalidFileException):
+        openjp2wrap.get_bitdeph(source)
--- uiucprescon/imagevalidate/openjp2wrap.cpp
+++ uiucprescon/imagevalidate/openjp2wrap.cpp
@@ -1,4 +1,6 @@
 #include "glue.h"
+#include "exceptions.h"
+
 #include <pybind11/pybind11.h>
 
 PYBIND11_MODULE(openjp2wrap, m){
@@ -7,5 +9,7 @@ PYBIND11_MODULE(openjp2wrap, m){
     m.def("open_jpeg_version", &open_jpeg_version, "Get the version of OpenJPEG built with");
     m.def("get_colorspace", &color_space, "get color space value from an image");
     m.def("get_bitdeph", &bitdepth, "get color bitdepth from an image");
+    pybind11::register_exception<InvalidFileException>(m, "InvalidFileException");
+
 }
 
