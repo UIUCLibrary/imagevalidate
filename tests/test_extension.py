0f08ee86f20c525eec86557db3c4a7c4cb191797
henry <henryborchers@yahoo.com>
2021-05-25 15:42:06 -0500

    jp2 expect 8 bit color depth

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -5,10 +5,10 @@ def test_dummy():
     print(openjp2wrap.open_jpeg_version())
 
 def test_get_colorspace_exists():
-    assert hasattr(openjp2wrap, "get_bitdeph")
+    assert hasattr(openjp2wrap, "get_bit_depth")
 
 def test_invalid_get_bitdeph_throws_exception():
     source = "fakefile.jp2"
     with pytest.raises(openjp2wrap.InvalidFileException) as excinfo:
-        openjp2wrap.get_bitdeph(source)
+        openjp2wrap.get_bit_depth(source)
     assert source in str(excinfo.value)
--- uiucprescon/imagevalidate/openjp2wrap.cpp
+++ uiucprescon/imagevalidate/openjp2wrap.cpp
@@ -8,7 +8,7 @@ PYBIND11_MODULE(openjp2wrap, m){
     options.enable_function_signatures();
     m.def("open_jpeg_version", &open_jpeg_version, "Get the version of OpenJPEG built with");
     m.def("get_colorspace", &color_space, "get color space value from an image");
-    m.def("get_bitdeph", &bitdepth, "get color bitdepth from an image");
+    m.def("get_bit_depth", &bitdepth, "get color bit depth from an image");
     pybind11::register_exception<InvalidFileException>(m, "InvalidFileException");
 
 }
