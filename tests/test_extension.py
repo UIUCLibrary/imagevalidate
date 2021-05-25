349b3bd04aaf98909e2b24f5eb999553e669bf61
henry <henryborchers@yahoo.com>
2021-05-24 22:28:00 -0500

    python has access to get_bitdeph

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -3,3 +3,7 @@ from uiucprescon.imagevalidate import openjp2wrap
 
 def test_dummy():
     print(openjp2wrap.open_jpeg_version())
+
+def test_get_colorspace_exists():
+    assert hasattr(openjp2wrap, "get_bitdeph")
+
--- uiucprescon/imagevalidate/openjp2wrap.cpp
+++ uiucprescon/imagevalidate/openjp2wrap.cpp
@@ -6,5 +6,8 @@ PYBIND11_MODULE(openjp2wrap, m){
     options.enable_function_signatures();
     m.def("open_jpeg_version", &open_jpeg_version, "Get the version of OpenJPEG built with");
     m.def("get_colorspace", &color_space, "get color space value from an image");
+    m.def("get_bitdeph", [](const std::string &file_name){
+        return bitdepth(file_name);
+        }, "get color bitdepth from an image");
 }
 
