2e7c8479ded8ba0b2620d8d16b2481a0c449191f
henry <henryborchers@yahoo.com>
2021-05-26 12:47:27 -0500

    variable 'info' is initialized

--- uiucprescon/imagevalidate/openjp2wrap.cpp
+++ uiucprescon/imagevalidate/openjp2wrap.cpp
@@ -3,7 +3,7 @@
 
 #include <pybind11/pybind11.h>
 
-PYBIND11_MODULE(openjp2wrap, m){
+PYBIND11_MODULE(openjp2wrap, m){ // NOLINT
     pybind11::options options;
     options.enable_function_signatures();
     m.def("open_jpeg_version", &open_jpeg_version, "Get the version of OpenJPEG built with");
