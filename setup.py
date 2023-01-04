bec325c4310b24c59e24b47e3da571b730ec9c3d
henry <henryborchers@yahoo.com>
2023-01-04 15:09:24 -0600

    removed deprecated parameter namespace_packages from setup

--- setup.py
+++ setup.py
@@ -119,7 +119,6 @@ setup(
         'uiucprescon.imagevalidate',
         "uiucprescon.imagevalidate.profiles"],
     test_suite="tests",
-    namespace_packages=["uiucprescon"],
     setup_requires=['pytest-runner'],
     install_requires=['py3exiv2bind>=0.1.9b4'],
     tests_require=['pytest'],
